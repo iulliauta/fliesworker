53067be1f92d522cff6d08cfaeee560afa562659
iulliauta <44622321+iulliauta@users.noreply.github.com>
2019-04-21 08:07:37 -0400

    Add files via upload

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -20,12 +20,24 @@ namespace Worker
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
+		string[] arrayZboruri = ["Londra, BritishAirlines - 2 ore, fara escala, 90 EURO",
+					 "Paris, AirFrance - 3 ore, fara escala, 100 EURO",
+                                         "Instanbul, TurkishAirlines - 1:30, fara escala, 80 EURO",
+                                         "Madrid, BlueAir - 4 ore, escala Paris, 220 EURO",
+                                         "Barcelona, WizzAir - 3 ore, fara escala, 200 EURO",
+                                         "Londra, BlueAir - 2 ore, fara escala, 95 EURO",
+                                         "Londra, WizzAir - 4 ore, escala Paris, 120 EURO",
+                                         "Praga, Tarom - 1:30, fara escala, 90 EURO",
+                                         "Viena, BlueAir - 2 ore, fara escala, 100 EURO",
+                                         "Venetia, AlItalia - 3 ore, fara escala, 120 EURO"];
+		string[] copyArrayZboruri = arrayZboruri;
+
                 // Keep alive is not implemented in Npgsql yet. This workaround was recommended:
                 // https://github.com/npgsql/npgsql/issues/1214#issuecomment-235828359
                 var keepAliveCommand = pgsql.CreateCommand();
                 keepAliveCommand.CommandText = "SELECT 1";
 
-                var definition = new { vote = "", voter_id = "" };
+                var definition = new { plecare = "", intoarcere = "", zbor = "", voter_id = "" };
                 while (true)
                 {
                     // Slow down to prevent CPU spike, only query each 100ms
@@ -37,11 +49,11 @@ namespace Worker
                         redisConn = OpenRedisConnection("redis");
                         redis = redisConn.GetDatabase();
                     }
-                    string json = redis.ListLeftPopAsync("votes").Result;
+                    string json = redis.ListLeftPopAsync("zboruri").Result;
                     if (json != null)
                     {
-                        var vote = JsonConvert.DeserializeAnonymousType(json, definition);
-                        Console.WriteLine($"Processing vote for '{vote.vote}' by '{vote.voter_id}'");
+                        var zbor = JsonConvert.DeserializeAnonymousType(json, definition);
+                        Console.WriteLine($"Processing zbor for '{zbor.plecare} - {zbor.intoarcere}' by '{zbor.voter_id}'");
                         // Reconnect DB if down
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
@@ -49,8 +61,28 @@ namespace Worker
                             pgsql = OpenDbConnection("Server=db;Username=postgres;");
                         }
                         else
-                        { // Normal +1 vote requested
-                            UpdateVote(pgsql, vote.voter_id, vote.vote);
+                        { // get 4 random flies
+			    Random ran = new Random();
+                            int index1 = ran.Next(0, arrayZboruri.Length);
+                            zbor.zbor = arrayZboruri[index1];
+			    arrayZboruri = arrayZboruri.Where((source, index) => index != index1).ToArray();
+                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
+
+			    int index2 = ran.Next(0, arrayZboruri.Length);
+			    zbor.zbor = arrayZboruri[index2];
+			    arrayZboruri = arrayZboruri.Where((source, index) => index != index2).ToArray();
+                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
+
+ 			    int index3 = ran.Next(0, arrayZboruri.Length);
+                            zbor.zbor = arrayZboruri[index3];
+			    arrayZboruri = arrayZboruri.Where((source, index) => index != index3).ToArray();
+                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
+
+			    int index4 = ran.Next(0, arrayZboruri.Length);
+                            zbor.zbor = arrayZboruri[index4];
+			    arrayZboruri = arrayZboruri.Where((source, index) => index != index4).ToArray();
+                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
+			    arrayZboruri = copyArrayZboruri;
                         }
                     }
                     else
@@ -93,9 +125,10 @@ namespace Worker
             Console.Error.WriteLine("Connected to db");
 
             var command = connection.CreateCommand();
-            command.CommandText = @"CREATE TABLE IF NOT EXISTS votes (
-                                        id VARCHAR(255) NOT NULL UNIQUE,
-                                        vote VARCHAR(255) NOT NULL
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS zboruri (
+                                        plecare VARCHAR(255),
+                                        intoarcere VARCHAR(255),
+                                        zbor VARCHAR(255)
                                     )";
             command.ExecuteNonQuery();
 
@@ -130,20 +163,21 @@ namespace Worker
                 .First(a => a.AddressFamily == AddressFamily.InterNetwork)
                 .ToString();
 
-        private static void UpdateVote(NpgsqlConnection connection, string voterId, string vote)
+        private static void UpdateVote(NpgsqlConnection connection, string plecare, string intoarcere, string zbor)
         {
             var command = connection.CreateCommand();
             try
             {
-                command.CommandText = "INSERT INTO votes (id, vote) VALUES (@id, @vote)";
-                command.Parameters.AddWithValue("@id", voterId);
-                command.Parameters.AddWithValue("@vote", vote);
+                command.CommandText = "INSERT INTO zboruri (plecare, intoarcere, zbor) VALUES (@plecare, @intoarcere, @zbor)";
+                command.Parameters.AddWithValue("@plecare", plecare);
+                command.Parameters.AddWithValue("@intoarcere", intoarcere);
+		command.Parameters.AddWithValue("@zbor", zbor);
                 command.ExecuteNonQuery();
             }
             catch (DbException)
             {
-                command.CommandText = "UPDATE votes SET vote = @vote WHERE id = @id";
-                command.ExecuteNonQuery();
+                //command.CommandText = "UPDATE votes SET vote = @vote WHERE id = @id";
+                //command.ExecuteNonQuery();
             }
             finally
             {
