884ce7592fbf0a3a04734983bcf32ac24f7a09be
iulliauta <44622321+iulliauta@users.noreply.github.com>
2019-04-21 10:25:18 -0400

    Add files via upload

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -12,6 +12,20 @@ namespace Worker
 {
     public class Program
     {
+	public string[] arrayZboruri = new string[] 
+		{"Londra, BritishAirlines - 2 ore, fara escala, 90 EURO",
+		 "Paris, AirFrance - 3 ore, fara escala, 100 EURO",
+                 "Instanbul, TurkishAirlines - 1:30, fara escala, 80 EURO",
+                 "Madrid, BlueAir - 4 ore, escala Paris, 220 EURO",
+                 "Barcelona, WizzAir - 3 ore, fara escala, 200 EURO",
+                 "Londra, BlueAir - 2 ore, fara escala, 95 EURO",
+                 "Londra, WizzAir - 4 ore, escala Paris, 120 EURO",
+                 "Praga, Tarom - 1:30, fara escala, 90 EURO",
+                 "Viena, BlueAir - 2 ore, fara escala, 100 EURO",
+                 "Venetia, AlItalia - 3 ore, fara escala, 120 EURO"};
+
+	public string[] copyArrayZboruri = arrayZboruri;
+
         public static int Main(string[] args)
         {
             try
@@ -20,24 +34,12 @@ namespace Worker
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
-		string[] arrayZboruri = ["Londra, BritishAirlines - 2 ore, fara escala, 90 EURO",
-					 "Paris, AirFrance - 3 ore, fara escala, 100 EURO",
-                                         "Instanbul, TurkishAirlines - 1:30, fara escala, 80 EURO",
-                                         "Madrid, BlueAir - 4 ore, escala Paris, 220 EURO",
-                                         "Barcelona, WizzAir - 3 ore, fara escala, 200 EURO",
-                                         "Londra, BlueAir - 2 ore, fara escala, 95 EURO",
-                                         "Londra, WizzAir - 4 ore, escala Paris, 120 EURO",
-                                         "Praga, Tarom - 1:30, fara escala, 90 EURO",
-                                         "Viena, BlueAir - 2 ore, fara escala, 100 EURO",
-                                         "Venetia, AlItalia - 3 ore, fara escala, 120 EURO"];
-		string[] copyArrayZboruri = arrayZboruri;
-
                 // Keep alive is not implemented in Npgsql yet. This workaround was recommended:
                 // https://github.com/npgsql/npgsql/issues/1214#issuecomment-235828359
                 var keepAliveCommand = pgsql.CreateCommand();
                 keepAliveCommand.CommandText = "SELECT 1";
 
-                var definition = new { plecare = "", intoarcere = "", zbor = "", voter_id = "" };
+                var definition = new { plecare = "", intoarcere = "", voter_id = "" };
                 while (true)
                 {
                     // Slow down to prevent CPU spike, only query each 100ms
@@ -49,7 +51,7 @@ namespace Worker
                         redisConn = OpenRedisConnection("redis");
                         redis = redisConn.GetDatabase();
                     }
-                    string json = redis.ListLeftPopAsync("zboruri").Result;
+                    string json = redis.ListLeftPopAsync("entries").Result;
                     if (json != null)
                     {
                         var zbor = JsonConvert.DeserializeAnonymousType(json, definition);
@@ -61,28 +63,8 @@ namespace Worker
                             pgsql = OpenDbConnection("Server=db;Username=postgres;");
                         }
                         else
-                        { // get 4 random flies
-			    Random ran = new Random();
-                            int index1 = ran.Next(0, arrayZboruri.Length);
-                            zbor.zbor = arrayZboruri[index1];
-			    arrayZboruri = arrayZboruri.Where((source, index) => index != index1).ToArray();
-                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
-
-			    int index2 = ran.Next(0, arrayZboruri.Length);
-			    zbor.zbor = arrayZboruri[index2];
-			    arrayZboruri = arrayZboruri.Where((source, index) => index != index2).ToArray();
-                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
-
- 			    int index3 = ran.Next(0, arrayZboruri.Length);
-                            zbor.zbor = arrayZboruri[index3];
-			    arrayZboruri = arrayZboruri.Where((source, index) => index != index3).ToArray();
-                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
-
-			    int index4 = ran.Next(0, arrayZboruri.Length);
-                            zbor.zbor = arrayZboruri[index4];
-			    arrayZboruri = arrayZboruri.Where((source, index) => index != index4).ToArray();
-                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere, zbor.zbor);
-			    arrayZboruri = copyArrayZboruri;
+                        { 
+                            UpdateVote(pgsql, zbor.plecare, zbor.intoarcere);
                         }
                     }
                     else
@@ -126,6 +108,7 @@ namespace Worker
 
             var command = connection.CreateCommand();
             command.CommandText = @"CREATE TABLE IF NOT EXISTS zboruri (
+					id VARCHAR(255),
                                         plecare VARCHAR(255),
                                         intoarcere VARCHAR(255),
                                         zbor VARCHAR(255)
@@ -163,21 +146,43 @@ namespace Worker
                 .First(a => a.AddressFamily == AddressFamily.InterNetwork)
                 .ToString();
 
-        private static void UpdateVote(NpgsqlConnection connection, string plecare, string intoarcere, string zbor)
+        private static void UpdateVote(NpgsqlConnection connection, string voterId,string plecare, string intoarcere)
         {
             var command = connection.CreateCommand();
+	    Random ran = new Random();
+            string[] randomZbor = new string[4];
+            int index1 = ran.Next(0, arrayZboruri.Length - 1);
+            randomZbor[0] = arrayZboruri[index1];
+            arrayZboruri = arrayZboruri.Where((source, index) => index != index1).ToArray();
+
+	    int index2 = ran.Next(0, arrayZboruri.Length - 1);
+	    randomZbor[1] = arrayZboruri[index2];
+	    arrayZboruri = arrayZboruri.Where((source, index) => index != index2).ToArray();
+                           
+	    int index3 = ran.Next(0, arrayZboruri.Length - 1);
+            randomZbor[2] = arrayZboruri[index3];
+            arrayZboruri = arrayZboruri.Where((source, index) => index != index3).ToArray();
+
+	    int index4 = ran.Next(0, arrayZboruri.Length - 1);
+            randomZbor[3] = arrayZboruri[index4];
+	    arrayZboruri = arrayZboruri.Where((source, index) => index != index4).ToArray();
+                         
+	    arrayZboruri = copyArrayZboruri;
             try
             {
-                command.CommandText = "INSERT INTO zboruri (plecare, intoarcere, zbor) VALUES (@plecare, @intoarcere, @zbor)";
-                command.Parameters.AddWithValue("@plecare", plecare);
-                command.Parameters.AddWithValue("@intoarcere", intoarcere);
-		command.Parameters.AddWithValue("@zbor", zbor);
-                command.ExecuteNonQuery();
+                int i;
+                for(i = 0; i < 4; i++) {
+               		command.CommandText = "INSERT INTO zboruri (plecare, intoarcere, zbor) VALUES (@plecare, @intoarcere, @zbor)";
+                	command.Parameters.AddWithValue("@plecare", plecare);
+                	command.Parameters.AddWithValue("@intoarcere", intoarcere);
+			command.Parameters.AddWithValue("@zbor", randomZbor[i]);
+                	command.ExecuteNonQuery();
+		}
             }
             catch (DbException)
             {
-                //command.CommandText = "UPDATE votes SET vote = @vote WHERE id = @id";
-                //command.ExecuteNonQuery();
+                command.CommandText = "UPDATE votes SET zbor = @zbor WHERE id = @id";
+                command.ExecuteNonQuery();
             }
             finally
             {
